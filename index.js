274f49d654bfb9f51073c200e8387255ed6728f3
Tejus Raghavendra <tejusraghavendra09@gmail.com>
2023-09-09 14:28:58 +0530

    ejs added working-3

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // / Add Express
 const express = require("express");
+const path = require("path");
 
 // Initialize Express
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-let image = "./images/1.png";
 // Create GET request
 app.get("/", (req, res) => {
     try {
+        let image = "./images/1.png";
         res.render("index", { image });
     } catch (error) {
         console.log("error:", error);
