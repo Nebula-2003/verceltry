8fad3d424f2d9504d3f1f3c8b8c1e8d2b3dba055
Tejus Raghavendra <tejusraghavendra09@gmail.com>
2023-09-09 11:43:12 +0530

    pkg json error solved commit-2

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const { v4 } = require("uuid");
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    res.send("<h1>DONE</h1>")
 });
 
 app.get("/api/item/:slug", (req, res) => {
